758685ec404212d98ba0310a60c06bdf21f15811
league <league@imac-14.attlocal.net>
2022-09-24 11:28:32 -0700

    Finished the riddler

--- src/_03_if_else/_5_pizza/pizza/pizza.pde
+++ src/_03_if_else/_5_pizza/pizza/pizza.pde
@@ -1,7 +1,17 @@
+PImage pepperoni;
 
 void setup() {
-    
+    pepperoni = loadImage("pepperoni.png");
+    size(500, 500);
 }
 void draw() {
-    
+    noStroke();
+    fill(#CE9C51);
+    ellipse(250, 250, 500, 500);
+    fill(#E35555);
+    ellipse(250, 250, 425, 425);
+    fill(#F0D988);
+    ellipse(250, 250, 400, 400);
+    image(pepperoni,200,200);
+    image(pepperoni,200,200);
 }
--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _04_int._1_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,17 +12,34 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String riddle = JOptionPane.showInputDialog("What can run but Cannot walk?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if(riddle.equals("river")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+			score += 1;
+		}
+				
 		// 5. Otherwise, say "wrong" and tell them the answer
 
+		else
+			JOptionPane.showMessageDialog(null, "WRONG");
+		
 		// 6. Add some more riddles
 
-		// 2. Make a pop up to show the score.
+		String riddle2 = JOptionPane.showInputDialog("What goes from Z to A?");
 		
+		if(riddle2.equals("zebra")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+			score += 1;
+		}	
+		
+		else 
+			JOptionPane.showMessageDialog(null, "WRONG");
+		
+		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, score);
 	}
 }
 
