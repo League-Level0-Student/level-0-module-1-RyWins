66c92a14caaa8c1766b3ee551700111ccb9b81e6
league <league@wts14-3.attlocal.net>
2022-09-29 19:54:24 -0700

    working on zombie eyes

--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -28,19 +28,33 @@ public class RobotInSpace extends KeyboardAdapter {
 	 * and rob.setAngle(angle) to change the direction of your Robot. //Do not add
 	 * code here - go to step 2
 	 */
-
+	
 	private void moveRobot(int keyPressed) {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
-		
+		System.out.println(keyPressed);
 		// 3. If the up arrow is pressed, move the Robot up the screen.
-		
+		rob.setSpeed(15);
+		if(keyPressed == 38){
+			rob.setAngle(0);
+			rob.move(2);
+		}
 		// 4. If the down arrow is pressed, move the Robot down.
-		
+		if(keyPressed == 40) {
+			rob.setAngle(180);
+			rob.move(2);
+		}
 		// 5. If the left arrow is pressed, make the Robot go left.
+		if(keyPressed == 37) {
+			rob.setAngle(270);
+			rob.move(2);
+		}
 		
 		// 6. If right is pressed, move the Robot right.
-		
+		if(keyPressed == 39) {
+			rob.setAngle(90);
+			rob.move(2);
+		}
 		// 7. Run your program and move the Robot to R2-D2 for a surprise!
 		
 	}
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,7 +1,13 @@
 
 void setup() {
-
+  size(500, 500);
+  PImage face = loadImage("zombieFace.jpeg");
+  face.resize(500, 500);
+  image(face, 0, 0);
 }
 void draw() {
-
+  noStroke();
+  fill(#FA0303);
+  ellipse(175, 198, 120, 120);
+  ellipse(350, 220, 130, 100);
 }
